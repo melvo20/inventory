be59aa469b6a94327f77b123d0a3507b12254b9e
Melvo <melvo20@yahoo.com>
2022-08-23 00:55:14 -0400

    Created calender page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,4 +7,7 @@ class MainController < ApplicationController
 
   def contact
   end
+
+  def calender
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'main#home'
+  get '/home', to: 'main#home'
   get '/about', to: 'main#about'
   get '/contact', to: 'main#contact'
+  get '/calender', to: 'main#calender'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
