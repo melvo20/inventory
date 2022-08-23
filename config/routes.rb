ef613f0c41c23d3333f1bdf0849cc29fd2755153
Melvo <melvo20@yahoo.com>
2022-08-23 00:44:06 -0400

    New Controler, Main, Home about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'main#home'
+  get '/about', to: 'main#about'
+  get '/contact', to: 'main#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
